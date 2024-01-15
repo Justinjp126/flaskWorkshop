21d214b2f10ae6f057b54ff1373e0db2fefcda27
Justin Puthenpurayil <jputhen@purdue.edu>
2024-01-15 18:22:40 -0500

    finish D in crud

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    # completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -35,6 +36,16 @@ def index():  # define route
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "error :("
 
 
 if __name__ == "__main__":
